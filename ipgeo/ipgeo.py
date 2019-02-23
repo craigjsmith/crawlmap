544fe51d4fd7f1bb5864ddd2eb73b8bc834717d7
Craig Smith <CraigJSmith@users.noreply.github.com>
2019-02-23 17:36:03 -0500

    Initial commit

---
+++ ipgeo/ipgeo.py
@@ -0,0 +1,25 @@
+import json
+from urllib.request import urlopen
+
+output = open("locations.txt", "w")
+
+ips = 'ips.txt'  
+with open(ips) as fp:  
+   line = fp.readline()
+   while line:
+        try:
+            print(line)
+            apicall = "http://api.ipstack.com/" + line.rstrip() + "?access_key=a8b8d0606d4a392c76c248be3dafc552"
+            print(apicall)
+            response = urlopen(apicall).read()
+        except:
+            pass
+
+        data = json.loads(response)
+        lat = data["latitude"]
+        lon = data["longitude"]
+        output.write(str(lat) + "," + str(lon) + "\n")
+
+        line = fp.readline()
+
+output.close()
